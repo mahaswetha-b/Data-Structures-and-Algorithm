a5d343030f5d5d87ec15f4a4ae8165e313d3507d
mahaswetha-b <mahaswethab10@gmail.com>
2026-01-22 21:57:56 +0530

    Checking Greates elements.c

---
+++ Additonal program/Greates elements.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    if(a>=b && a>=c && a>=d)
+    {
+        printf("%d\n",a);
+    }
+    else if(b>=a && b>=c &b>=d)
+    {
+        printf("%d\n",b);
+    }
+    else if(c>=a && c>=b && c>=d)
+    {
+        printf("%d\n",c);
+    }
+    else
+    {
+        printf("%d",d);
+    }
+    return 0;
+}
