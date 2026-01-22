7a40e6ebe9f3f355ac75b4e8843e95e86642cc01
mahaswetha-b <mahaswethab10@gmail.com>
2026-01-22 21:49:45 +0530

    Create odd or even.c

---
+++ Additonal program/odd or even.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("%d:even",n);
+    }
+    else
+    {
+        printf("%d:Odd",n);
+    }
+    return 0;
+}
